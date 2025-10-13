7c3f89419f58693851aa98eb037edd36d9a1593c
Hung <ls@hung.vn>
2025-10-13 22:42:09 +0700

    Customize container

--- app/install.sh
+++ app/install.sh
@@ -7,6 +7,7 @@ set -euo pipefail
 main() {
     declare_variables || { log_failed "Failed to declare variables."; exit 1; }
     clean_temporary_directory || { log_failed "Failed to clean temporary directory."; exit 1; }
+    parse_config_values || { log_failed "Failed to parse config values."; exit 1; }
     generate_background_image || { log_failed "Failed to generate background image."; exit 1; }
     generate_selected_item_pixmap || { log_failed "Failed to generate selected item pixmap."; exit 1; }
     generate_menu_item_icons || { log_failed "Failed to generate menu item icons."; exit 1; }
@@ -39,6 +40,30 @@ clean_temporary_directory() {
     mkdir -p "$TEMP_DIR/icons"
 }
 
+parse_config_values() {
+    log_info "Parsing config values..."
+
+    PARSED_CONTAINER_WIDTH="$CONTAINER_WIDTH"
+    if [[ "$CONTAINER_WIDTH" =~ ^[0-9]+%$ ]]; then
+        PARSED_CONTAINER_WIDTH="$(( $SCREEN_WIDTH * ${CONTAINER_WIDTH%\%} / 100 ))"
+    fi
+
+    PARSED_CONTAINER_HEIGHT="$CONTAINER_HEIGHT"
+    if [[ "$CONTAINER_HEIGHT" =~ ^[0-9]+%$ ]]; then
+        PARSED_CONTAINER_HEIGHT="$(( $SCREEN_HEIGHT * ${CONTAINER_HEIGHT%\%} / 100 ))"
+    fi
+
+    PARSED_CONTAINER_TOP="$CONTAINER_TOP"
+    if [[ "$CONTAINER_TOP" =~ ^[0-9]+%$ ]]; then
+        PARSED_CONTAINER_TOP="$(( $SCREEN_HEIGHT * ${CONTAINER_TOP%\%} / 100 ))"
+    fi
+
+    PARSED_CONTAINER_LEFT="$CONTAINER_LEFT"
+    if [[ "$CONTAINER_LEFT" =~ ^[0-9]+%$ ]]; then
+        PARSED_CONTAINER_LEFT="$(( $SCREEN_WIDTH * ${CONTAINER_LEFT%\%} / 100 ))"
+    fi
+}
+
 generate_background_image() {
     log_info "Generating background image..."
 
@@ -52,30 +77,19 @@ generate_background_image() {
     fi
 
     if [[ "$CONTAINER_TYPE" == "blur" ]]; then
-        create_container_blurred_image || return 1
+        create_blurred_image_container || return 1
     elif [[ "$CONTAINER_TYPE" == "solid" ]]; then
-        create_container_solid_color || return 1
+        create_solid_color_container || return 1
     else
         log_error "Invalid container type '$CONTAINER_TYPE'."
         return 1
     fi
 
-    if ! scale_image_by_factor "$TEMP_DIR/container.png" "$TEMP_DIR/container.png" 10; then
-        log_error "Failed to scale image up to avoid aliasing."
-        return 1
+    if [[ "$CONTAINER_BORDER_RADIUS" -gt 0 ]]; then
+        create_border_radius_for_container || return 1
     fi
 
-    if ! create_rounded_corners "$TEMP_DIR/container.png" "$TEMP_DIR/container.png" "$(( 50 * 10 ))"; then
-        log_error "Failed to create rounded corners for container."
-        return 1
-    fi
-
-    if ! scale_image_by_factor "$TEMP_DIR/container.png" "$TEMP_DIR/container.png" 0.1; then
-        log_error "Failed to scale image down as original size."
-        return 1
-    fi
-
-    if ! overlay_image "$TEMP_DIR/background.png" "$TEMP_DIR/container.png" "$TEMP_DIR/background.png" 576 324; then
+    if ! overlay_image "$TEMP_DIR/background.png" "$TEMP_DIR/container.png" "$TEMP_DIR/background.png" "$PARSED_CONTAINER_LEFT" "$PARSED_CONTAINER_TOP"; then
         log_error "Failed to overlay container on background image."
         return 1
     fi
@@ -95,21 +109,38 @@ create_solid_color_background() {
     fi
 }
 
-create_container_blurred_image() {
-    if ! crop_image "$TEMP_DIR/background.png" "$TEMP_DIR/container.png" "iw*0.4" "ih*0.4" "iw*0.3" "ih*0.3"; then
+create_blurred_image_container() {
+    if ! crop_image "$TEMP_DIR/background.png" "$TEMP_DIR/container.png" "$PARSED_CONTAINER_WIDTH" "$PARSED_CONTAINER_HEIGHT" "$PARSED_CONTAINER_LEFT" "$PARSED_CONTAINER_TOP"; then
         log_error "Failed to crop image to create container."
         return 1
     fi
 
-    if ! blur_image "$TEMP_DIR/container.png" "$TEMP_DIR/container.png" 30 -0.05 0.95; then
+    if ! blur_image "$TEMP_DIR/container.png" "$TEMP_DIR/container.png" "$CONTAINER_BLUR_SIGMA" "$CONTAINER_BLUR_BRIGHTNESS" "$CONTAINER_BLUR_CONTRAST"; then
         log_error "Failed to make container blurred."
         return 1
     fi
 }
 
-create_container_solid_color() {
-    if ! create_solid_color_image "$TEMP_DIR/container.png" "#313244" 768 432; then
+create_solid_color_container() {
+    if ! create_solid_color_image "$TEMP_DIR/container.png" "$CONTAINER_COLOR" "$PARSED_CONTAINER_WIDTH" "$PARSED_CONTAINER_HEIGHT"; then
         log_error "Failed to create solid color image for container."
+        return 1
+    fi
+}
+
+create_border_radius_for_container() {
+    if ! scale_image_by_factor "$TEMP_DIR/container.png" "$TEMP_DIR/container.png" 10; then
+        log_error "Failed to scale image up to avoid aliasing."
+        return 1
+    fi
+
+    if ! create_rounded_corners "$TEMP_DIR/container.png" "$TEMP_DIR/container.png" "$(( $CONTAINER_BORDER_RADIUS * 10 ))"; then
+        log_error "Failed to create rounded corners for container."
+        return 1
+    fi
+
+    if ! scale_image_by_factor "$TEMP_DIR/container.png" "$TEMP_DIR/container.png" 0.1; then
+        log_error "Failed to scale image down as original size."
         return 1
     fi
 }
