387d240d40bc2f342d4ff03d636c332d49b9be12
Hung <ls@hung.vn>
2025-10-17 09:53:05 +0700

    Show verbose output

--- app/install.sh
+++ app/install.sh
@@ -5,10 +5,11 @@
 set -euo pipefail
 
 main() {
+    parse_arguments "$@" || return 1
     check_required_commands_exist || return 1
     declare_variables || { log_failed "Failed to declare variables."; return 1; }
-    clean_temporary_directory || { log_failed "Failed to clean temporary directory."; return 1; }
     parse_config_values || { log_failed "Failed to parse config values."; return 1; }
+    clean_temporary_directory || { log_failed "Failed to clean temporary directory."; return 1; }
     download_background_image_if_not_set || { log_failed "Failed to download background image."; return 1; }
     generate_background_image || { log_failed "Failed to generate background image."; return 1; }
     generate_selected_item_pixmap || { log_failed "Failed to generate selected item pixmap."; return 1; }
@@ -23,6 +24,39 @@ main() {
     log_ok "Done."
 }
 
+parse_arguments() {
+    log_info "Parsing arguments..."
+
+    shift
+    while [[ $# -gt 0 ]]; do
+        case "$1" in
+            -h|--help)
+                show_usage
+                exit 0
+                ;;
+            -v|--verbose)
+                VERBOSE="yes"
+                ;;
+            *)
+                log_error "Invalid option '$1'."
+                show_usage
+                exit 1
+                ;;
+        esac
+        shift
+    done
+
+    [[ "$VERBOSE" == "yes" ]] && log_ok "Parsed arguments successfully." || true
+}
+
+show_usage() {
+    echo "Usage: $APP_NAME_LOWER install [OPTIONS]"
+    echo ""
+    echo "Options:"
+    echo "    -h, --help        Show help"
+    echo "    -v, --verbose     Verbose output"
+}
+
 check_required_commands_exist() {
     log_info "Checking required commands exist..."
 
@@ -32,6 +66,8 @@ check_required_commands_exist() {
     check_command_exists "envsubst" || { log_error "Command 'envsubst' not found."; return 1; }
     check_command_exists "tar" || { log_error "Command 'tar' not found."; return 1; }
     check_one_of_commands_exists "update-grub" "grub-mkconfig" "grub2-mkconfig" || { log_error "Command 'update-grub', 'grub-mkconfig' or 'grub2-mkconfig' not found."; return 1; }
+
+    [[ "$VERBOSE" == "yes" ]] && log_ok "All required commands exist." || true
 }
 
 declare_variables() {
@@ -46,7 +82,8 @@ declare_variables() {
 
     GRUB_THEME_DIR="$GRUB_THEMES_DIR/$APP_NAME_LOWER"
 
-    # Colors
+    [[ "$VERBOSE" == "yes" ]] && log_info "Setting default config values..." || true
+
     [ -z "$CONTAINER_COLOR" ] && CONTAINER_COLOR="$THEME_BACKGROUND_COLOR" || true
     [ -z "$ITEM_COLOR" ] && ITEM_COLOR="$THEME_TEXT_COLOR" || true
     [ -z "$SELECTED_ITEM_COLOR" ] && SELECTED_ITEM_COLOR="$THEME_BACKGROUND_COLOR" || true
@@ -55,15 +92,8 @@ declare_variables() {
     [ -z "$PROGRESS_BAR_FOREGROUND_COLOR" ] && PROGRESS_BAR_FOREGROUND_COLOR="$THEME_ACCENT_COLOR" || true
     [ -z "$PROGRESS_BAR_BACKGROUND_COLOR" ] && PROGRESS_BAR_BACKGROUND_COLOR="$THEME_BACKGROUND_COLOR" || true
     [ -z "$PROGRESS_BAR_BORDER_COLOR" ] && PROGRESS_BAR_BORDER_COLOR="$THEME_BACKGROUND_COLOR" || true
-}
 
-clean_temporary_directory() {
-    log_info "Cleaning temporary directory..."
-
-    rm -rf "$TEMP_DIR"/{*,.[!.]*}
-    mkdir -p "$TEMP_DIR"
-    mkdir -p "$TEMP_DIR/icons"
-    mkdir -p "$TEMP_DIR/fonts"
+    [[ "$VERBOSE" == "yes" ]] && log_ok "Declared variables successfully." || true
 }
 
 parse_config_values() {
@@ -88,16 +118,30 @@ parse_config_values() {
     if [[ "$CONTAINER_LEFT" =~ ^[0-9]+%$ ]]; then
         PARSED_CONTAINER_LEFT="$(( $SCREEN_WIDTH * ${CONTAINER_LEFT%\%} / 100 ))"
     fi
+
+    [[ "$VERBOSE" == "yes" ]] && log_ok "Parsed config values successfully." || true
+}
+
+clean_temporary_directory() {
+    log_info "Cleaning temporary directory..."
+
+    rm -rf "$TEMP_DIR"/{*,.[!.]*}
+    mkdir -p "$TEMP_DIR"
+    mkdir -p "$TEMP_DIR/icons"
+    mkdir -p "$TEMP_DIR/fonts"
+
+    [[ "$VERBOSE" == "yes" ]] && log_ok "Cleaned temporary directory successfully." || true
 }
 
 download_background_image_if_not_set() {
-    log_info "Downloading background image if not set..."
-
     if [[ "$BACKGROUND_TYPE" == "file" && -z "$BACKGROUND_FILE" ]]; then
-        local url="https://raw.githubusercontent.com/lshung/grubify-assets/master/background.jpg"
+        log_info "Downloading background image..."
 
+        local url="https://raw.githubusercontent.com/lshung/grubify-assets/master/background.jpg"
         BACKGROUND_FILE="$TEMP_DIR/background.jpg"
         curl -s -L -o "$BACKGROUND_FILE" "$url" || return 1
+
+        [[ "$VERBOSE" == "yes" ]] && log_ok "Downloaded background image successfully." || true
     fi
 }
 
@@ -133,53 +177,85 @@ generate_background_image() {
 }
 
 fit_background_image_to_screen() {
+    [[ "$VERBOSE" == "yes" ]] && log_info "Scaling background image to fit screen..." || true
+
     if ! scale_image_to_fit_screen "$BACKGROUND_FILE" "$TEMP_DIR/background.png" "$SCREEN_WIDTH" "$SCREEN_HEIGHT"; then
         log_error "Failed to scale image to fit screen."
         return 1
     fi
+
+    [[ "$VERBOSE" == "yes" ]] && log_ok "Scaled background image to fit screen successfully." || true
 }
 
 create_solid_color_background() {
+    [[ "$VERBOSE" == "yes" ]] && log_info "Creating solid color background..." || true
+
     if ! create_solid_color_image "$TEMP_DIR/background.png" "$BACKGROUND_COLOR" "$SCREEN_WIDTH" "$SCREEN_HEIGHT"; then
         log_error "Failed to create solid color image for background."
         return 1
     fi
+
+    [[ "$VERBOSE" == "yes" ]] && log_ok "Created solid color background successfully." || true
 }
 
 create_blurred_image_container() {
+    [[ "$VERBOSE" == "yes" ]] && log_info "Cropping background image to create container..." || true
+
     if ! crop_image "$TEMP_DIR/background.png" "$TEMP_DIR/container.png" "$PARSED_CONTAINER_WIDTH" "$PARSED_CONTAINER_HEIGHT" "$PARSED_CONTAINER_LEFT" "$PARSED_CONTAINER_TOP"; then
         log_error "Failed to crop image to create container."
         return 1
     fi
 
+    [[ "$VERBOSE" == "yes" ]] && log_ok "Cropped background image to create container successfully." || true
+
+    [[ "$VERBOSE" == "yes" ]] && log_info "Making container blurred..." || true
+
     if ! blur_image "$TEMP_DIR/container.png" "$TEMP_DIR/container.png" "$CONTAINER_BLUR_SIGMA" "$CONTAINER_BLUR_BRIGHTNESS" "$CONTAINER_BLUR_CONTRAST"; then
         log_error "Failed to make container blurred."
         return 1
     fi
+
+    [[ "$VERBOSE" == "yes" ]] && log_ok "Made container blurred successfully." || true
 }
 
 create_solid_color_container() {
+    [[ "$VERBOSE" == "yes" ]] && log_info "Creating solid color container..." || true
+
     if ! create_solid_color_image "$TEMP_DIR/container.png" "$CONTAINER_COLOR" "$PARSED_CONTAINER_WIDTH" "$PARSED_CONTAINER_HEIGHT"; then
         log_error "Failed to create solid color image for container."
         return 1
     fi
+
+    [[ "$VERBOSE" == "yes" ]] && log_ok "Created solid color container successfully." || true
 }
 
 create_border_radius_for_container() {
+    [[ "$VERBOSE" == "yes" ]] && log_info "Scaling container image up to avoid aliasing..." || true
+
     if ! scale_image_by_factor "$TEMP_DIR/container.png" "$TEMP_DIR/container.png" 10; then
-        log_error "Failed to scale image up to avoid aliasing."
+        log_error "Failed to scale image up."
         return 1
     fi
+
+    [[ "$VERBOSE" == "yes" ]] && log_ok "Scaled image up successfully." || true
+
+    [[ "$VERBOSE" == "yes" ]] && log_info "Creating rounded corners for container..." || true
 
     if ! create_rounded_corners "$TEMP_DIR/container.png" "$TEMP_DIR/container.png" "$(( $CONTAINER_BORDER_RADIUS * 10 ))"; then
         log_error "Failed to create rounded corners for container."
         return 1
     fi
 
+    [[ "$VERBOSE" == "yes" ]] && log_ok "Created rounded corners for container successfully." || true
+
+    [[ "$VERBOSE" == "yes" ]] && log_info "Scaling container image down as original size..." || true
+
     if ! scale_image_by_factor "$TEMP_DIR/container.png" "$TEMP_DIR/container.png" 0.1; then
-        log_error "Failed to scale image down as original size."
+        log_error "Failed to scale image down."
         return 1
     fi
+
+    [[ "$VERBOSE" == "yes" ]] && log_ok "Scaled image down successfully." || true
 }
 
 generate_selected_item_pixmap() {
@@ -191,6 +267,8 @@ generate_selected_item_pixmap() {
         sed -i "s/fill=\".*\"/fill=\"$SELECTED_ITEM_BACKGROUND_COLOR\"/g" "$TEMP_DIR/$file_name.svg" || return 1
         scale_image_with_values "$TEMP_DIR/$file_name.svg" "$TEMP_DIR/$file_name.png" -1 "$ITEM_HEIGHT" || return 1
     done
+
+    [[ "$VERBOSE" == "yes" ]] && log_ok "Generated selected item pixmap successfully." || true
 }
 
 generate_menu_item_icons() {
@@ -199,15 +277,19 @@ generate_menu_item_icons() {
     for file in "$APP_TEMPLATES_ICONS_DIR/$ICON_THEME"/*.svg; do
         export_menu_item_icon "$file" "$TEMP_DIR/icons/$(basename "$file" .svg).png" -1 "$ICON_SIZE" || return 1
     done
+
+    [[ "$VERBOSE" == "yes" ]] && log_ok "Generated menu item icons successfully." || true
 }
 
 download_and_convert_fonts() {
     log_info "Downloading and converting fonts..."
 
-    download_font "unifont"
-    download_font "terminus"
-    convert_font_to_pf2_format "unifont"
-    convert_font_to_pf2_format "terminus"
+    download_font "unifont" || return 1
+    download_font "terminus" || return 1
+    convert_font_to_pf2_format "unifont" || return 1
+    convert_font_to_pf2_format "terminus" || return 1
+
+    [[ "$VERBOSE" == "yes" ]] && log_ok "Downloaded and converted fonts successfully." || true
 }
 
 substitute_variables_in_theme_txt() {
@@ -226,6 +308,8 @@ substitute_variables_in_theme_txt() {
     export PROGRESS_BAR_ALIGN PROGRESS_BAR_FOREGROUND_COLOR PROGRESS_BAR_BACKGROUND_COLOR PROGRESS_BAR_BORDER_COLOR
 
     envsubst < "$TEMP_DIR/theme.tmp" > "$TEMP_DIR/theme.txt"
+
+    [[ "$VERBOSE" == "yes" ]] && log_ok "Substituted variables in file 'theme.txt' successfully." || true
 }
 
 copy_assets_to_theme_dir() {
@@ -239,6 +323,8 @@ copy_assets_to_theme_dir() {
     sudo cp "$TEMP_DIR"/select_*.png "$GRUB_THEME_DIR"/
     sudo cp -r "$TEMP_DIR/icons" "$GRUB_THEME_DIR"/
     sudo cp "$TEMP_DIR/fonts"/*.pf2 "$GRUB_THEME_DIR"/ || true
+
+    [[ "$VERBOSE" == "yes" ]] && log_ok "Copied assets to theme directory successfully." || true
 }
 
 edit_file_etc_default_grub() {
@@ -258,18 +344,24 @@ edit_file_etc_default_grub() {
     comment_setting_in_etc_default_grub "GRUB_BACKGROUND" || return 1
     comment_setting_in_etc_default_grub "GRUB_COLOR_NORMAL" || return 1
     comment_setting_in_etc_default_grub "GRUB_COLOR_HIGHLIGHT" || return 1
+
+    [[ "$VERBOSE" == "yes" ]] && log_ok "Edited file '/etc/default/grub' successfully." || true
 }
 
 update_grub_configuration() {
     log_info "Updating GRUB configuration..."
 
     update_grub_config >/dev/null 2>&1 || return 1
+
+    [[ "$VERBOSE" == "yes" ]] && log_ok "Updated GRUB configuration successfully." || true
 }
 
 cleanup() {
     log_info "Cleaning up..."
 
     rm -rf "$TEMP_DIR"
+
+    [[ "$VERBOSE" == "yes" ]] && log_ok "Cleaned up successfully." || true
 }
 
 main "$@"
--- app/utils/font.sh
+++ app/utils/font.sh
@@ -15,15 +15,21 @@ download_font() {
 }
 
 download_font_unifont() {
-    local url="https://unifoundry.com/pub/unifont/unifont-17.0.01/font-builds/unifont-17.0.01.otf"
+    [[ "$VERBOSE" == "yes" ]] && log_info "Downloading font 'Unifont'..." || true
 
-    curl -L -s -o "$TEMP_DIR/fonts/unifont.otf" "$url" || return 1
+    local url="https://unifoundry.com/pub/unifont/unifont-17.0.01/font-builds/unifont-17.0.01.otf"
+    curl -L -s -o "$TEMP_DIR/fonts/unifont.otf" "$url" || { log_failed "Failed to download font 'Unifont'."; return 1; }
+
+    [[ "$VERBOSE" == "yes" ]] && log_ok "Downloaded font 'Unifont' successfully." || true
 }
 
 download_font_terminus() {
-    local url="https://sourceforge.net/projects/terminus-font/files/terminus-font-4.49/terminus-font-4.49.1.tar.gz/download"
+    [[ "$VERBOSE" == "yes" ]] && log_info "Downloading font 'Terminus'..." || true
 
-    curl -L -s -o "$TEMP_DIR/fonts/terminus.tar.gz" "$url" || return 1
+    local url="https://sourceforge.net/projects/terminus-font/files/terminus-font-4.49/terminus-font-4.49.1.tar.gz/download"
+    curl -L -s -o "$TEMP_DIR/fonts/terminus.tar.gz" "$url" || { log_failed "Failed to download font 'Terminus'."; return 1; }
+
+    [[ "$VERBOSE" == "yes" ]] && log_ok "Downloaded font 'Terminus' successfully." || true
 }
 
 convert_font_to_pf2_format() {
@@ -37,21 +43,31 @@ convert_font_to_pf2_format() {
 }
 
 convert_unifont_to_pf2_format() {
+    [[ "$VERBOSE" == "yes" ]] && log_info "Converting font 'Unifont' to PF2 format..." || true
+
     local sizes=(16 18 20 22 24 26 28 30 32)
 
     for size in "${sizes[@]}"; do
         grub-mkfont -o "$TEMP_DIR/fonts/unifont-${size}.pf2" -s "$size" "$TEMP_DIR/fonts/unifont.otf"
     done
+
+    [[ "$VERBOSE" == "yes" ]] && log_ok "Converted font 'Unifont' to PF2 format successfully." || true
 }
 
 convert_terminus_to_pf2_format() {
-    local sizes=(12 14 16 18 20 22 24 28 32)
+    [[ "$VERBOSE" == "yes" ]] && log_info "Converting font 'Terminus' to PF2 format..." || true
 
-    tar -xzf "$TEMP_DIR/fonts/terminus.tar.gz" -C "$TEMP_DIR/fonts" || return 1
+    [[ "$VERBOSE" == "yes" ]] && log_info "Extracting file 'terminus.tar.gz'..." || true
+    tar -xzf "$TEMP_DIR/fonts/terminus.tar.gz" -C "$TEMP_DIR/fonts" || { log_failed "Failed to extract file 'terminus.tar.gz'."; return 1; }
+    [[ "$VERBOSE" == "yes" ]] && log_ok "Extracted file 'terminus.tar.gz' successfully." || true
+
     local extracted_dir=$(find "$TEMP_DIR/fonts" -maxdepth 1 -mindepth 1 -type d | head -n 1)
+    local sizes=(12 14 16 18 20 22 24 28 32)
 
     for size in "${sizes[@]}"; do
         grub-mkfont -o "$TEMP_DIR/fonts/terminus-${size}.pf2" -s "$size" "$extracted_dir/ter-u${size}n.bdf"
         grub-mkfont -o "$TEMP_DIR/fonts/terminus-bold-${size}.pf2" -s "$size" "$extracted_dir/ter-u${size}b.bdf"
     done
+
+    [[ "$VERBOSE" == "yes" ]] && log_ok "Converted font 'Terminus' to PF2 format successfully." || true
 }
